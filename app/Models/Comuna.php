631bcf139e0463202de9c89bab94b59e92105802
juan <mjuanpablo523@gmail.com>
2025-05-27 09:26:38 -0500

    implementacion de datos comuna

--- app/Models/Comuna.php
+++ app/Models/Comuna.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comuna extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'tb_comuna';
+    protected $primaryKey = 'comu_codi';
+    public $timestamps = false;
+
 }
